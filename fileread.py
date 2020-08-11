7bc5e9bc22558c6952f732b4c51ce59d7f87109a
Rashidat <rashidatjay@gmail.com>
2020-08-11 20:59:45 +0100

    translator script

--- fileread.py
+++ fileread.py
@@ -4,4 +4,7 @@ translator=Translator(to_lang="fr")
 with open('your_file.txt','r+')as fileread:
     text=fileread.read()
     translator=translator.translate(text)
+    with open('translated.txt', 'w')as translated_text:
+        translated_text.write(translator)
+
 print(translator)
